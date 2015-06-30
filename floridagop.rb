197799d68c189e64d56477245d331288a5f037e6
AthleteInvictus <cantucade@gmail.com>
2015-06-29 23:51:35 -0400

    Added Florida

---
+++ floridagop.rb
@@ -0,0 +1,52 @@
+require 'csv'
+require 'statistics2'
+require 'pollster'
+require 'uri'
+include Pollster
+
+puts "Enter a name:"
+puts "Options are Rubio, Bush, Walker, Carson, Trump"
+answer = gets.chomp
+puts "Enter another name, same options:"
+answer2 = gets.chomp
+
+poll = Poll.where(:chart => '2016-florida-presidential-republican-primary').first
+responses = poll.questions.detect { |question| question.chart == '2016-florida-presidential-republican-primary' }.responses
+dude = responses.detect { |response| response[:choice] == "#{answer}" }
+dude2 = responses.detect { |response| response[:choice] == "#{answer2}" }
+
+difference = (dude[:value] - dude2[:value]).to_f
+firstpart = (difference/3).to_f
+tdist = (Statistics2.tdist(2,firstpart)).to_f
+tdistabs = (Statistics2.tdist(2,firstpart.abs)).to_f
+percent = tdist*100
+percentabs = tdistabs*100
+opercent = (1-tdist)*100
+opercentabs = (1-tdistabs)*100
+
+case 
+  when difference < 0
+    puts " "
+    puts "#{opercentabs.round(2)}""%" " #{answer}."
+    puts "#{100-opercentabs.round(2)}""%" " #{answer2}."
+    puts "Based on latest poll at Pollster.com"
+  when difference > 0
+    puts " "
+    puts "#{(opercentabs.round(2))}""%" " #{answer2}."
+    puts "#{(percentabs.round(2))}""%" " #{answer}."
+    puts "Based on latest poll at Pollster.com"
+  when difference = 0
+    puts "50% #{answer}."
+    puts "50% #{answer2}."
+    puts "Based on latest poll at Pollster.com"
+  else 
+	puts "An error occurred."
+	puts "Seek help at:"
+	puts "github.com/AthleteInvictus"
+  end
+
+runcsv = File.open("csv/floridagop.csv", "w+")
+  runcsv.puts (opercent.round(2)).abs
+  runcsv.puts "#{answer2}"
+  runcsv.puts (percent.round(2)).abs
+  runcsv.puts "#{answer}"
